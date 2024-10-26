9603562d58dc226e1b43e45519b08075d61d9b3f
Igor Makis <makis@Igors-MacBook-Pro.local>
2024-10-26 07:09:52 +0200

    Modified for 13.0 and sdk-ios 12.14.0

--- scripts/build_mobile_messaging_framework.sh
+++ scripts/build_mobile_messaging_framework.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo "Building MobileMessaging.xcframework with Carthage started..."
-cd plugins/com-infobip-plugins-mobilemessaging/libs/ios/
-carthage update --cache-builds --use-xcframeworks
+
+# Navigate to the Carthage directory
+cd plugins/com-infobip-plugins-mobilemessaging/libs/ios/ || exit
+
+# Set the iOS deployment target to 13.0
+export IPHONEOS_DEPLOYMENT_TARGET=13.0
+
+# Update Carthage with cache builds and xcframeworks
+# Add the specific platform to avoid building for unwanted platforms
+carthage update --platform iOS --cache-builds --use-xcframeworks
+
 echo "Building MobileMessaging.xcframework with Carthage finished."
-cd -
+
+# Navigate back to the original directory
+cd - || exit
